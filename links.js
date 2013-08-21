efa2ba0cf390b7b5b7d91fa448fccb2ad7bbf818
Adrian Ottiker <adrian@ottiker.com>
2013-08-21 18:15:57 +0200

    created basic mono module structure

---
+++ links.js
@@ -0,0 +1,15 @@
+var devConfig = {
+    
+};
+
+function init (config) {
+    var self = this;
+    
+    // TODO only for dev
+    self.config = devConfig;
+
+    console.log('crud-links init');
+}
+
+module.exports = init;
+
