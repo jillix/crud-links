5814212cc2eb0c2a41ff82776a582d4ac26a496d
Adrian Ottiker <adrian@ottiker.com>
2013-08-22 17:02:04 +0200

    work in progress, module is not ready for use

--- links.js
+++ links.js
@@ -1,15 +1,45 @@
 M.wrap('github/jillix/crud-links/dev/links.js', function (require, module, exports) {
+
+var Events = require('github/jillix/events');
+
 var devConfig = {
     
+    // connect external modules
+    "listen": {
+        "data_list_templates": {
+            "selectionChanged": [
+                { "emit": "setTemplate" }
+            ]
+        },
+        "data_form": {
+            "dataSet": [
+                { "emit": "setData" }
+            ]
+        }
+    }
 };
+
+function setData (data) {
+    console.log(data);
+}
+
+function setTemplate (template) {
+    console.log(template);
+}
 
 function init (config) {
     var self = this;
     
     // TODO only for dev
     self.config = devConfig;
-
-    console.log('crud-links init');
+    
+    self.on('setData', setData);
+    self.on('setTemplate', setTemplate);
+    
+    // listen to external events
+    Events.call(self, self.config);
+    
+    self.emit('ready');
 }
 
 module.exports = init;
