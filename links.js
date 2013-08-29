f99ae7a57a1cf47db0a84f36b3f039abcab15317
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-08-29 20:09:47 +0200

    added cloning of default filter and table configs from the configuration

--- links.js
+++ links.js
@@ -2,37 +2,50 @@ M.wrap('github/jillix/crud-links/dev/links.js', function (require, module, expor
 
 var Events = require('github/jillix/events');
 
+// function to clone a JSON object (it wil not clone function, dates, etc.)
+function cloneJSON(obj) {
+    // basic type deep copy
+    if (obj === null || obj === undefined || typeof obj !== 'object')  {
+        return obj
+    }
+    // array deep copy
+    if (obj instanceof Array) {
+        var cloneA = [];
+        for (var i = 0; i < obj.length; ++i) {
+            cloneA[i] = cloneJSON(obj[i]);
+        }
+        return cloneA;
+    }
+    // object deep copy
+    var cloneO = {};
+    for (var i in obj) {
+        cloneO[i] = cloneJSON(obj[i]);
+    }
+    return cloneO;
+}
+
 function setData (data) {
-    console.log(data);
+    //console.log(data);
 }
 
 function setTemplate (template) {
     var self = this;
     
-    if (template && template.links instanceof Array) {
-        var filterDefault = self.config.clones.filter;
-        var tableDefault = self.config.clones.table;
-        for (var i = 0, l = template.links.length; i < l; ++i) {
-            
-            if (!template.links[i].tableOnly) {
-                M.clone('#linksContainer', 'data_filter', '_links_' + i, filterDefault.config);
-            }
-            M.clone('#linksContainer', 'data_table', '_links_' + i, tableDefault.config);
+    if (!template || !template.links instanceof Array) {
+        return;
+    }
 
-            /*
-                TODO:
-                - define ui and interaction
-                    - ui config for crud-links
-                - define event flow
-                - create config for filter
-                    - take schema from link
-                - create config for table
-                    - columns are defined in the filter query (fields)
-                - clone data_filter module
-                - clone data_table module
-                - add relations to accordion
-            */
+    for (var i = 0, l = template.links.length; i < l; ++i) {
+        // we clone the default configurations because they will be modified by the modules
+        var filterConfig = cloneJSON(self.config.clones.filter.config) || {};
+        var tableConfig = cloneJSON(self.config.clones.table.config) || {};
+
+        if (!template.links[i].tableOnly && filterConfig.ui) {
+            delete filterConfig.ui;
         }
+
+        M.clone('#linksContainer', self.config.clones.filter.miid, '_' + template._id, filterConfig);
+        M.clone('#linksContainer', self.config.clones.filter.miid, '_' + template._id, tableConfig);
     }
 }
 
